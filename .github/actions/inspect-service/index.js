084888098f250bc6bdd69c086222ab4ab83f9df1
Victor Rojas <vicko16@gmail.com>
2022-06-07 07:01:10 -0500

    inspect process env

--- .github/actions/inspect-service/index.js
+++ .github/actions/inspect-service/index.js
@@ -1,9 +1,9 @@
-// const core = require('@actions/core');
+const core = require('@actions/core');
 const exec = require('@actions/exec');
 
 const inspect = async function() {
     try {
-        await exec.exec('echo', [Object.keys(proces.env).join(', ')]);
+        await exec.exec('echo', [Object.keys(process.env).join(', ')]);
     } catch (error) {
         core.setFailed(error.message);
     }
